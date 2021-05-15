901a1923323914659f5f125453d41f2a31b195ce
andrea <pred_95@live.it>
2021-05-15 13:41:23 +0200

    Fix region picker in create new offer

--- mobile/src/components/CreateOfferComponent/index.js
+++ mobile/src/components/CreateOfferComponent/index.js
@@ -1,39 +1,68 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {View, Text} from 'react-native';
 import styles from './styles';
 import Container from '../common/Container';
 import Input from '../common/Input';
 import CustomButton from '../common/CustomButton';
 import {REGIONS} from '../../constants/regions';
-import {Picker} from '@react-native-picker/picker';
 import RNPickerSelect from 'react-native-picker-select';
-import colors from '../../assets/themes/colors';
 
-const CreateOfferComponent = () => {
+const CreateOfferComponent = ({onChangeText, form}) => {
+  const [region, setRegion] = useState('');
+  var regions = [];
+  REGIONS.map(region => {
+    regions.push({label: region, value: region});
+  });
+  console.log(`regions`, regions);
   return (
     <View style={styles.container}>
       <Container>
-        <Input label="Product" placeholder="Enter name of the product" />
-        <Input label="Shop" placeholder="Enter name of the shop" />
-        <Input label="City" placeholder="Enter city of the shop" />
+        <Input
+          label="Product"
+          placeholder="Enter name of the product"
+          onChangeText={value => {
+            onChangeText({name: 'product', value: value});
+          }}
+        />
+        <Input
+          label="Shop"
+          placeholder="Enter name of the shop"
+          onChangeText={value => {
+            onChangeText({name: 'shop', value: value});
+          }}
+        />
+        <Input
+          label="City"
+          placeholder="Enter city of the shop"
+          onChangeText={value => {
+            onChangeText({name: 'city', value: value});
+          }}
+        />
         <View style={styles.pickerContainer}>
           <Text style={styles.pickerLabel}>Region</Text>
           <View style={styles.pickerWrapper}>
             <View style={styles.inputContainer}>
               <RNPickerSelect
+                style={{inputAndroid: {color: 'black'}}}
                 useNativeAndroidPickerStyle={true}
                 placeholder={{label: 'Select a region', value: null}}
-                onValueChange={value => console.log(`region: `, value)}
-                items={[
-                  {label: 'Abruzzo', value: 'Abruzzo'},
-                  {label: 'Basilicata', value: 'Basilicata'},
-                  {label: 'Calabria', value: 'Calabria'},
-                ]}
+                onValueChange={value => {
+                  setRegion(value);
+                  onChangeText({name: 'region', value: value});
+                }}
+                items={regions}
+                value={region}
               />
             </View>
           </View>
         </View>
-        <Input label="Price" placeholder="Enter price of the product" />
+        <Input
+          label="Price"
+          placeholder="Enter price of the product"
+          onChangeText={value => {
+            onChangeText({name: 'price', value: value});
+          }}
+        />
         <Input label="Image" placeholder="Upload image of the product" />
 
         <CustomButton primary title="Submit" />
--- mobile/src/screens/CreateOffer/index.js
+++ mobile/src/screens/CreateOffer/index.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Text, View} from 'react-native';
-import CreateOfferComponent from '../../components/CreateOfferComponent'
+import CreateOfferComponent from '../../components/CreateOfferComponent';
 
 const CreateOffer = () => {
-  return (
-    <CreateOfferComponent />
-  );
+  const [form, setForm] = useState({});
+
+  const onChangeText = ({name, value}) => {
+    setForm({...form, [name]: value});
+  };
+
+  return <CreateOfferComponent onChangeText={onChangeText} form={form} />;
 };
 
 export default CreateOffer;
