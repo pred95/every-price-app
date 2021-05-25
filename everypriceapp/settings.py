36c48561f836d170378f89e3e44bba3ae256cf7d
andrea <pred_95@live.it>
2021-05-25 17:53:34 +0200

    Deploy frontend

--- everypriceapp/settings.py
+++ everypriceapp/settings.py
@@ -18,7 +18,7 @@ import django_heroku
 
 django_heroku.settings(locals())
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().root.root
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
