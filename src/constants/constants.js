8acbc65adb1eb8027f410e4ffac1bfabdc4540cd
andrea <pred_95@live.it>
2021-05-25 17:00:50 +0200

    Deploy to heroku

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,4 +1,4 @@
-export const BACKEND_URL = "http://everypriceapp.herokuapp.com/";
+export const BACKEND_URL = "https://everypriceapp.herokuapp.com/";
 // export const BACKEND_URL = "http://localhost:8000/";
 export const REGIONS = [
   "Abruzzo",
